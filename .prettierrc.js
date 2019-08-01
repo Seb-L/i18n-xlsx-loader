2fb0c5ddb76996cfd55dfc59b214801eb453b27c
Seb.L <sebastien.lombard75@gmail.com>
2019-08-01 22:17:31 +0200

    chore(build): init project with build, lint and test stack

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: 'typescript',
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  tabWidth: 2,
+  semi: false,
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # i18n-xlsx-loader
+
 Directly import an XLSX file in your project as source for i18n locales messages.
